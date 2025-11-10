ce10e6794543786ff32e77992df77ad092ae9fdf
DavidPr03 <tdpd1234@outlook.es>
2025-11-10 13:01:02 -0600

    ProfileScreen1

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -62,8 +62,6 @@ export default function ProfileScreen({ route }) {
               </Pressable>
             ))}
           </VStack>
-
-          
         </VStack>
       </SafeAreaView>
     </>
